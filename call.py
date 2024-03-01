a1d5f16e8f1a8c639b1e107d071b81cdb1c3ed2d
NigelYeoTW <nigelyeotongwei@gmail.com>
2024-03-01 13:22:56 +0000

    Code for http call that will be done from turtlebot after subscribing to a trigger that is made when the http calling NFC is detected

---
+++ call.py
@@ -0,0 +1,23 @@
+#need to install pip3 with sudo apt-get install python3-pip
+#use pip3 to install requests module
+#click most outside button to boot/reset the ESP32
+#syntax for http POST for door
+
+import requests
+# Define new data to create
+new_data = {
+    "action": "openDoor",
+    "parameters": {"robotId": "30"}
+}
+
+# The API endpoint to communicate with
+# This is specific http to our own ESP32 when connected to Nigel hotspot
+url_post = "http://192.168.42.87/openDoor"
+
+# A POST request to tthe API
+# Each request can only be made with a 30? sec interval if not no feedback
+post_response = requests.post(url_post, json=new_data)
+
+# Print the response
+post_response_json = post_response.json()
+print(post_response_json)
